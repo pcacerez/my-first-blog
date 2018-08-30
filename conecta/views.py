657acbc2e4e7e92bc1f4496cb6882d2ce4aa201e
Pablo <pcacerez@gmail.com>
2018-08-30 18:23:06 +0200

    modificamos templates to display posts from database.

--- conecta/views.py
+++ conecta/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone # Utilizaremos published_date__lte
+from .models import Post
+
+#.models = el punto indica que estamos en el directorio mimso que realizamos la llamada, en este caso llamos a
+# models desde views.
 
 def post_list(request):
-    return render(request, 'conecta/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #ordenamos los objetos.
+    return render(request, 'conecta/post_list.html', {'posts':posts}) #'posts' = nombre de la varialbe que enviamos.

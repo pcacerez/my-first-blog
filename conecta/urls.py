f8c2a2ee5cf1d27ade5f0dfc9aa70beb33d8d32d
Pablo <pcacerez@gmail.com>
2018-08-31 14:13:36 +0200

    Nueva ventana detail.html

--- conecta/urls.py
+++ conecta/urls.py
@@ -4,4 +4,15 @@ from .import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    """
+        post/ = Significa que después del comienzo, la dirección URL debe contener la palabra post y /
+        (?P<pk>[0-9]+) = Significa que Django llevará todo lo que coloques aquí y lo transferirá
+        a una vista como una variable llamada pk. [0-9]
+        también nos dice que sólo puede ser un número, no una letra (todo debería estar entre 0 y 9).
+        + significa que tiene que haber uno o más dígitos. Entonces
+        / - entonces necesitamos / de nuevo
+        $ - ¡"el final"!
+        Example= http://127.0.0.1:8000/post/5/
+    """
 ]
--- conecta/views.py
+++ conecta/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone # Utilizaremos published_date__lte
 from .models import Post
 
@@ -8,3 +8,6 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #ordenamos los objetos.
     return render(request, 'conecta/post_list.html', {'posts':posts}) #'posts' = nombre de la varialbe que enviamos.
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})

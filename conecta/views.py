bddf376ff104c63f4593ebb41520f7063b92c425
Pablo <pcacerez@gmail.com>
2018-09-01 17:46:25 +0200

    Editar post

--- conecta/views.py
+++ conecta/views.py
@@ -36,7 +36,7 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.author = 'Paquito Chocolatero'
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
